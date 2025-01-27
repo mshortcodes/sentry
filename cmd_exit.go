e366e1bbf5eda466103862a3658d3fcb11ca86b9
mshortcodes <mshortcodes@gmail.com>
2025-01-26 17:37:57 -0800

    edit exit msg

--- cmd_exit.go
+++ cmd_exit.go
@@ -7,6 +7,13 @@ import (
 
 func cmdExit(s *state) error {
 	fmt.Println("\tExiting Sentry...")
+
+	if s.user != nil {
+		fmt.Printf("\tGoodbye, %s!\n\n", s.user.Username)
+		os.Exit(0)
+		return nil
+	}
+
 	fmt.Print("\tGoodbye!\n\n")
 	os.Exit(0)
 	return nil
