ebe535fe35da50f9fbc9af07fe0a5efc07439916
mshortcodes <mshortcodes@gmail.com>
2025-01-26 18:06:28 -0800

    update reset cmd

--- cmd_reset.go
+++ cmd_reset.go
@@ -13,13 +13,13 @@ func cmdReset(s *state) error {
 		s.scanner.Scan()
 		input := s.scanner.Text()
 		input = cleanInput(input)
+
 		switch input {
 		case "y":
 			fmt.Print("\tResetting passwords...\n")
 			if err := s.db.Reset(s.user.Id); err != nil {
 				return fmt.Errorf("failed to reset passwords: %v", err)
 			}
-
 			fmt.Print("\tPasswords have been reset!\n\n")
 			return nil
 		case "n":
