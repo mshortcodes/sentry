6630b01040b45b2634179e9a4f968eaa13480c84
mshortcodes <mshortcodes@gmail.com>
2025-02-04 17:51:06 -0800

    update errors

--- internal/crypt/crypt.go
+++ internal/crypt/crypt.go
@@ -4,7 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"fmt"
+	"errors"
 	"golang.org/x/crypto/scrypt"
 )
 
@@ -43,7 +43,7 @@ func Decrypt(ciphertext, key, nonce []byte) (string, error) {
 func GenerateNonce() ([]byte, error) {
 	nonce := make([]byte, 12)
 	if _, err := rand.Read(nonce); err != nil {
-		return nil, fmt.Errorf("error generating nonce: %v", err)
+		return nil, errors.New("error generating nonce")
 	}
 	return nonce, nil
 }
@@ -51,7 +51,7 @@ func GenerateNonce() ([]byte, error) {
 func GenerateKey(password, salt []byte) ([]byte, error) {
 	key, err := scrypt.Key(password, salt, 32768, 8, 1, 32)
 	if err != nil {
-		return nil, fmt.Errorf("error generating key: %v", err)
+		return nil, errors.New("error generating key")
 	}
 	return key, nil
 }
@@ -59,7 +59,7 @@ func GenerateKey(password, salt []byte) ([]byte, error) {
 func GenerateSalt() ([]byte, error) {
 	salt := make([]byte, 8)
 	if _, err := rand.Read(salt); err != nil {
-		return nil, fmt.Errorf("error generating salt: %v", err)
+		return nil, errors.New("error generating salt")
 	}
 	return salt, nil
 }
